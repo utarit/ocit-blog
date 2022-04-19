140d3d068e64ab1b9745d3ed85202f3ce3fc4f65
Mert Akça <mert.akca@plentific.com>
2022-04-19 13:04:58 +0300

    added insta icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import { InstagramIcon } from "./InstagramIcon";
 
 const CATEGORIES = [
   {
@@ -15,7 +16,7 @@ const CATEGORIES = [
 const Header = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8 md:flex md:justify-between">
+      <div className="border-b w-full border-blue-400 py-8 flex justify-between">
         <div className="block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
@@ -23,14 +24,17 @@ const Header = () => {
             </span>
           </Link>
         </div>
-        <div className="hidden md:block">
-          {CATEGORIES.map((category) => (
-            <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className="mt-2 align-middle text-white ml-4 font-semibold cursor-pointer hover:text-red-700 transition duration-700">
-                {category.name}
-              </span>
-            </Link>
-          ))}
+        <div className="md:flex items-flex pt-3">
+          <InstagramIcon />
+          <div className="hidden md:block ">
+            {CATEGORIES.map((category) => (
+              <Link key={category.slug} href={`/category/${category.slug}`}>
+                <span className="text-white ml-4 font-semibold cursor-pointer hover:text-red-700 transition duration-700">
+                  {category.name}
+                </span>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
